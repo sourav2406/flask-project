99909f8ce5ba91790a658200c020c21fe02422d7
Sourav Mandal <sourav2406@gmail.com>
2017-11-27 18:26:14 +0530

    jinja added

--- user_app/view.py
+++ user_app/view.py
@@ -3,6 +3,6 @@ from flask import request, render_template
 
 @userApp.route('/')
 def index():
-    user = request.args.get('user','Sourav')
-    return render_template('index.html',user=user)
+    #user = request.args.get('user','Sourav')
+    return render_template('index.html')
     #return "<h3>Working</h3>"
