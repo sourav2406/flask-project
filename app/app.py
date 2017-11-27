24a3daf5aa9b94ce168937daef56bc8acb4ac3a0
Sourav Mandal <sourav2406@gmail.com>
2017-11-27 16:30:31 +0530

    added config for flask app

--- app/app.py
+++ app/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object('config.DevelopmentConfig')
 
 @app.route('/')
 def index():
-    return "<h1>Simple App!!!</h1>"
+    return "<h1>Simple App1 !!!</h1>"
 
 if __name__=='__main__':
     app.run(debug=True)
